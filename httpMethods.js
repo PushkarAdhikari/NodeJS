4d0843639df37b3804147031c7b059583162f7fa
Pushkar Adhikari <adhikari.1990@gmail.com>
2026-03-18 22:55:27 +0530

    update http-methods file

--- httpMethods.js
+++ httpMethods.js
@@ -3,13 +3,15 @@ const fs = require('fs');
 const url = require('url');
 
 
-const myServer = http.createServer((req, res) => {
+const myHandler = (req, res) => {
     if (req.url === '/favicon.ico') return null;
     console.log('Request received');
     const log = `${Date.now()} ${req.method} ${req.url} New Request received \n`;
     const myUrl = url.parse(req.url, true);
     fs.appendFile('./log.txt', log, (err, data) => {
         switch (myUrl.pathname) {
+            // Every case will have create all the methods (GET, POST, PUT, DELETE) and handle them accordingly
+            // Here come the ExpressJS and other frameworks which will make our life easier by handling all the methods and routes for us
             case '/':
                 if (req.method === 'GET') res.end('Hello from Homepage');
                 break;
@@ -24,6 +26,7 @@ const myServer = http.createServer((req, res) => {
                 if (req.method === 'GET') {
                     res.end('This is the signup form')
                 } else if (req.method === 'POST') {
+                    //DB logic to save the form data
                     res.end('Form submitted successfully')
                 };
                 break;
@@ -32,7 +35,9 @@ const myServer = http.createServer((req, res) => {
         }
 
     });
-});
+}
+
+const myServer = http.createServer(myHandler);
 const PORT = 4001;
 myServer.listen(PORT, (req, res) => {
     console.log(`Server is listening on port ${PORT}`);
